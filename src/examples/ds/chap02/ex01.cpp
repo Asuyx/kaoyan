8b69efa8c8d2ccb368ad006aed834a72b0516e56
ClazyChen <clazychen@gmail.com>
2021-07-04 17:07:04 +0800

    2.1 向量（2）
    向量的扩容、缩容、插入
    增加了一下对于复杂度记号的说明

--- src/examples/ds/chap02/ex01.cpp
+++ src/examples/ds/chap02/ex01.cpp
@@ -1,4 +1,24 @@
 // 问题2.1 - 向量扩容
 // 分析合适的方式让向量扩容
 
+#include <iostream>
+#include "vector.h"
+using namespace std;
+
 // 步骤1 - 改变向量的容量
+
+// 步骤2 - 设计扩容算法
+// 算法2.1B1 - 固定扩容
+template<typename T>
+void expandByAP(const Vector<T>& V, int d) {
+    V.expand([=](int m) -> { return m + d; });
+}
+// 算法2.1B2 - 比例扩容
+template<typename T>
+void expandByGP(const Vector<T>& V, double q) {
+    V.expand([=](int m) -> { return (int)(m * q); });
+}
+
+int main() {
+    return 0;
+}
--- src/headers/ds/vector.h
+++ src/headers/ds/vector.h
@@ -10,11 +10,19 @@ using namespace std;
 
 typedef int Rank;
 
+// 默认的扩容策略（加倍扩容）
+static int expandBy2(int m) {
+    return m << 1;
+}
+
 template <typename T>
 class Vector {
     T* _data;      // 向量所基于的数组
     int _capacity; // 向量的容量
     int _size;     // 向量的规模
+
+    static function<int(int)> capacity_expand_strategy = expandBy2; // 向量扩容的默认方法
+
 public:
     int capacity() { return _capacity; }
     int size() { return _size; }
@@ -36,6 +44,10 @@ public:
     }
 
     void set_capacity(int new_capacity); // 算法2.1（向量扩容）
+    void expand(function<int(int)> strategy = nullptr); // 向量自动扩容
+
+    void insert(T e, Rank r); // 算法2.2A - 向量插入元素
+    void insert(const Vector<T>& V, Rank r); // 算法2.3A - 向量合并
 };
 
 // 算法2.1A（改变向量的容量）
@@ -48,6 +60,37 @@ void Vector<T>::set_capacity(int new_capacity) {
     delete[] old;
 }
 
+// 算法2.1B - 基本框架
+template <typename T>
+void Vector<T>::expand(function<int(int)> strategy) {
+    if (strategy == nullptr) {
+        strategy = capacity_expand_strategy;
+    }
+    set_capacity(strategy(_capacity));
+}
+
+// 算法2.2A - 向量插入元素（单元）
+template <typename T>
+void Vector<T>::insert(T e, Rank r) {
+    if (r < 0) { r = _size - r; } // 仿Python的记法，如果r是负数，从后向前计数
+    if (_size + 1 > _capacity) { expand(); }    // 插入后会超出容量，需要扩容
+    arrayCopy(_data+r+1, _data+r, _size-r, -1); // 从后向前，依次移动V[r:n]中的每个元素
+    _data[r] = e;
+    ++_size; // 更新向量的规模
+}
+
+// 算法2.3A - 向量插入元素（批量）
+template <typename T>
+void Vector<T>::insert(const Vector<T>& V, Rank r) {
+    if (r < 0) { r = _size - r; } // 仿Python的记法，如果r是负数，从后向前计数
+    int new_size = _size + V._size;                   // 计算插入之后的向量规模
+    if (new_size > _capacity) { expand([=](int m) -> int {
+        return max(new_size, capacity_expand_strategy(m));
+    }); }        // 插入后会超出容量，需要扩容。这里省略指定的capacity_expand_strategy
+    arrayCopy(_data+r+V._size, _data+r, _size-r, -1); // 从后向前，依次移动V[r:n]中的每个元素
+    arrayCopy(_data+r, V._data, V._size);             // 依次插入V1中的元素
+    _size = new_size; // 更新向量的规模
+}
 
 //  ----------------------------------以下内容不在笔记正文中----------------------------
 
